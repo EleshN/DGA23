4e0a0fe41dd41e1d042c7b2a7b42421f1e1290a7
AnjelicaB <hmni.anjelica@gmail.com>
2023-09-24 19:38:58 -0400

    Added basic fields

---
+++ Assets/Scripts/Animal/Animal.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    [SerializeField] Rigidbody rd; 
+    
+    public int animalHealth;
+    public int animalDamage;
+    public enum animalEmotion {
+        Emotionless, // emoless
+        Love, // emo
+        Anger // emoless
+    }
+
+    [Header("Speeds")]
+    [SerializeField] float emolessSpeed;
+    [SerializeField] float emoSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
