af82822abddb0905e399872071e0bd9be7a7430f
Luke <llwn786@gmail.com>
2023-09-24 19:55:47 -0400

    added comment to test access

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// urmother
+
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
