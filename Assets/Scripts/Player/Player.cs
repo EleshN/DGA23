6d7fcdba1dc237e3bc925ebcf74bbfb48838650a
Luke <llwn786@gmail.com>
2023-10-01 15:33:03 -0400

    Implement Player script
    First commit for boilerplate player code, needs to be finalized and coordinated with the gun script to work as intended.
    
    The player should be able to:
    -move
    -change ammo
    -shoot

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,20 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-// urmotherlolasd
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int[] ammo;
+    public Gun gun;
+    public int ammoIndex;
+
+    public Text ammoTypeText;  // displays the current ammo type
+    public Text ammoCountText; // displays the current ammo count
+
+    public float moveSpeed = 6.9f;
+
+    private string[] ammoNames = { "Love", "Anger" }; // index 0 is Love and index 1 is Anger
+
+    private void Start()
     {
-        
+        UpdateUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Inputs();
+        Move();
+        Scroll();
+    }
+
+    private void Inputs()
+    {
+        if (Input.GetMouseButton(0)) // 0 represents left mouse button
+        {
+            if (ammo[ammoIndex] > 0)
+            {
+                gun.Fire();
+                ammo[ammoIndex]--;
+                UpdateUI();
+            }
+            else
+            {
+                // play an empty gun sound or something here idk
+            }
+        }
+    }
+
+
+    private void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical).normalized;
+
+        transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void Scroll()
+    {
+        // weapon switching here
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            ammoIndex = (ammoIndex + 1) % ammo.Length;
+        }
+        else if (Input.mouseScrollDelta.y < 0)
+        {
+            ammoIndex = (ammoIndex - 1 + ammo.Length) % ammo.Length;
+        }
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        ammoTypeText.text = ammoNames[ammoIndex];
+        ammoCountText.text = ammo[ammoIndex].ToString();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // Reduce ammo
+            if (ammo[ammoIndex] > 0)
+            {
+                ammo[ammoIndex]--;
+                UpdateUI();
+            }
+            else
+            {
+                // what happens when ammo is 0
+            }
+        }
     }
 }
+
