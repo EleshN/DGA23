1537a5d0560fd8329a8dde2b24172607b1ac63ab
AnjelicaB <hmni.anjelica@gmail.com>
2023-10-01 16:13:34 -0400

    Implemented IDamageable

--- Assets/Scripts/IDamageable.cs
+++ Assets/Scripts/IDamageable.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+/// <summary>
+/// This interface will only be implemented by the animal and robot
+/// </summary>
+public interface IDamageable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    float Health
+    void TakeDamage(float damageAmount)
+    void Die()
 }
