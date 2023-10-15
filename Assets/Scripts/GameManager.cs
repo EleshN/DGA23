7d89d903a2155f356c5481314f223d113f7a9924
Jerry Chen <jerry.w.chen.official@gmail.com>
2023-10-15 16:28:09 -0400

    Win/Lose Conditions + Base Trackers

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,9 +6,11 @@ public class GameManager : MonoBehaviour
 {
 
     private static GameManager _Instance;
-
+    public int LevelNumber = 1;
     public List<Animal> Animals { get; private set; }
     public List<Animal> Enemies { get; private set; }
+    public List<PlayerBase> PlayerBases { get; private set; }
+    public List<EnemyBase> EnemyBases { get; private set; }
 
     public static GameManager Instance
     {
@@ -25,18 +27,21 @@ public class GameManager : MonoBehaviour
         _Instance = this;
         Animals = new();
         Enemies = new();
+        PlayerBases = new();
+        EnemyBases = new();
 
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (len(PlayerBases) == 0) Debug.Log("Game Over");
+        if (len(EnemyBases) == 0) Debug.Log("You Win");
     }
 }
