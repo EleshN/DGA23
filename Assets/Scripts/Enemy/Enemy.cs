3b236bb9b96d9789705e27e5978f7a1c44ed2a88
Crystal Jin <kstjxy@gmail.com>
2023-10-29 11:04:54 -0400

    add state to enemy

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,12 +5,25 @@ using UnityEngine.AI;
 
 public abstract class Enemy : MonoBehaviour, IDamageable
 {
+
+    public enum State
+    {
+        SPWAN,
+        WANDER,
+        CHASE,
+        ATTACK
+    }
+
+    [SerializeField] State state;
     [SerializeField] protected NavMeshAgent agent;
     [SerializeField] Rigidbody rb;
     [SerializeField] float speed;
     [SerializeField] float maxHealth;
     [SerializeField] protected float currentHealth;
     [SerializeField] protected float robotDamage;
+    [SerializeField] protected float enemyDamage;
+    [SerializeField] protected float attackCountDown;
+    [SerializeField] protected float currentAtackTime;
 
     IDamageable targetState;
     Transform targetTransform;
@@ -26,21 +39,86 @@ public abstract class Enemy : MonoBehaviour, IDamageable
     void Start()
     {
         currentHealth = maxHealth;
+        currentAtackTime = attackCountDown;
         GameManager.Instance.Register(this);
+        state = State.SPWAN;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (targetTransform == null || !targetState.isDamageable())
-        {
-            selectNewTarget();
+        currentAtackTime -= Update.deltaTime;
+        changeStateIfAcceptable();
+        switch(state){
+            case State.WANDER:
+                selectNewTarget();
+                break;
+
+            case State.CHASE:
+                Move(targetTransform.position);
+                break;
+            
+            case State.Attack:
+                attack();
+                break;
+            
+            default:
+                break;
         }
-        // just stay still if there is no more targets on map, the game must end.
-        if (targetTransform != null)
-        {
-            Move(targetTransform.position);
-            Attack();
+        if (currentAtackTime <= 0){
+            currentAtackTime = attackCountDown;
+        }
+
+        // if (targetTransform == null || !targetState.isDamageable())
+        // {
+        //     selectNewTarget();
+        // }
+        // // just stay still if there is no more targets on map, the game must end.
+        // if (targetTransform != null)
+        // {
+        //     Move(targetTransform.position);
+        //     Attack();
+        // }
+    }
+
+    public void changeStateIfAcceptable(){
+        switch(state){
+            case State.SPWAN:
+                state = State.WANDER;
+                break;
+
+            case State.WANDER:
+                if (targetTransform != null && targetState.isDamageable())
+                {
+                    if (currentAtackTime <= 0){
+                        state = state.ATTACK;
+                    }else{
+                        state = state.CHASE;
+                    }
+                }
+                break;
+
+            case State.CHASE:
+                if (targetTransform == null || !targetState.isDamageable())
+                {
+                    state = state.WANDER;
+                } else if (currentAtackTime <= 0){
+                    state = state.ATTACK;
+                }
+                break;
+            
+            case State.ATTACK:
+                if (targetTransform == null || !targetState.isDamageable())
+                {
+                    state = state.WANDER;
+                } else{
+                    state = state.CHASE;
+                }
+                break;
+
+            default:
+                break;
+
         }
     }
 
