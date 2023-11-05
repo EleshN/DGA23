7cfd1224e6510d3878922a07a9ec4704dba3fe7b
Jerry Chen <jerry.w.chen.official@gmail.com>
2023-11-05 16:07:25 -0500

    Implemented damage indication material swaps for PlayerBase

--- Assets/Scripts/PlayerBase/PlayerBase.cs
+++ Assets/Scripts/PlayerBase/PlayerBase.cs
@@ -7,18 +7,47 @@ public class PlayerBase : MonoBehaviour, IDamageable
     public float HP;
     public GameObject EnemyBaseObject;
 
-    public void Start()
+    // Reference to the DamageIndicator component
+    private DamageIndicator damageIndicator;
+
+    void Start()
     {
         GameManager.Instance.Register(this);
+
+        // Get the DamageIndicator component attached to the same GameObject
+        damageIndicator = GetComponent<DamageIndicator>();
+
+        // You could also choose to add DamageIndicator component if it doesn't exist already
+        if (damageIndicator == null)
+        {
+            damageIndicator = gameObject.AddComponent<DamageIndicator>();
+        }
     }
+
     public void TakeDamage(float amount)
     {
+        if (!isDamageable()) return;
+
         HP -= amount;
+
+        // Call IndicateDamage on the DamageIndicator component
+        if (damageIndicator != null)
+        {
+            damageIndicator.IndicateDamage();
+        }
+
         if (HP <= 0)
         {
             GameManager.Instance.Unregister(this);
             Instantiate(EnemyBaseObject, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
+    }
+
+    public bool isDamageable()
+    {
+        // Implementation for isDamageable
+        // Return true or false based on the PlayerBase's current state
+        return true;
     }
 }
