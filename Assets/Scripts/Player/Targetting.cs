d4483df72d707887cdedf45cac9ab3a899fb51fd
Luke <llwn786@gmail.com>
2023-11-05 17:56:17 -0500

    Implement aiming reticle
    - Created a material for laser reticle
    - Aiming now respects the y-axis, so unlocked y-axis in Targetting.cs
    - aiming reticle now casts on to surface properly

--- Assets/Scripts/Player/Targetting.cs
+++ Assets/Scripts/Player/Targetting.cs
@@ -4,84 +4,82 @@ using UnityEngine;
 
 public class Targetting : MonoBehaviour
 {
-    [Tooltip("Make sure the ground has the ground layer mask")]
-    [SerializeField] private LayerMask groundMask;
+    // [Tooltip("Make sure the ground has the ground layer mask")]
+    // [SerializeField] private LayerMask groundMask;
     [Tooltip("Assign this to the transform of the gun")]
     [SerializeField] private Transform gunTransform;
-    [Tooltip("Assign a prefab for the reticle")]
-    [SerializeField] private GameObject reticlePrefab;
     [Tooltip("Assign a LineRenderer for the laser sight")]
     [SerializeField] private LineRenderer laserSight;
+    [Tooltip("Red Dot GameObject")]
+    [SerializeField] private GameObject redDotPrefab;
 
     private Camera mainCamera;
-    private GameObject reticle;
+    private GameObject redDotInstance;
 
     private void Start()
     {
         mainCamera = Camera.main;
-        reticle = Instantiate(reticlePrefab); // Instantiate the reticle at the start
         laserSight.enabled = false; // Starts with the laser sight disabled until we hit something
+        // Create the red dot instance and hide it initially
+        redDotInstance = Instantiate(redDotPrefab);
+        redDotInstance.SetActive(false);
     }
 
     private void Update()
     {
         AimUpdate();
-        ReticleUpdate();
+        AimAssist();
     }
 
     private void AimUpdate()
     {
-        var (success, position) = GetMousePosition();
+        var (success, hitInfo) = GetMousePosition();
         if (success)
         {
             // Calculate the direction
-            var direction = position - transform.position;
+            var direction = hitInfo.point - transform.position;
 
             // Ignore the height difference.
             // direction.y = 0;
 
             // Make the transform look in the direction, but only rotate around the y-axis.
-            transform.rotation = Quaternion.Euler(0, Quaternion.LookRotation(direction).eulerAngles.y, 0);
+            transform.rotation = Quaternion.Euler(0, Quaternion.LookRotation(direction).eulerAngles.y, 0);  
         }
     }
 
-    private void ReticleUpdate()
+    private void AimAssist()
     {
-        var (success, position) = GetMousePosition();
+        var (success, hitInfo) = GetMousePosition();
         if (success)
         {
-            // The Raycast hit something, show the reticle and laser sight.
-            // reticle.SetActive(true);
+            // The Raycast hit something, show the laser sight.
             laserSight.enabled = true;
-
-            // Reticle is positioned where the cursor is.
-            // reticle.transform.position = position;
-
-            // Draw laser sight from the gun tip to the cursor position.
             laserSight.SetPosition(0, gunTransform.position);
-            laserSight.SetPosition(1, position);
+            laserSight.SetPosition(1, hitInfo.point);
+
+            // Show the red dot at the hit position
+            redDotInstance.SetActive(true);
+            redDotInstance.transform.position = hitInfo.point;
+
+            redDotInstance.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
         }
         else
         {
-            // The Raycast did not hit anything, hide reticle and laser.
-            // reticle.SetActive(false);
+            // The Raycast did not hit anything, hide the laser and reticle.
             laserSight.enabled = false;
+            redDotInstance.SetActive(false);
         }
     }
 
-    private (bool success, Vector3 position) GetMousePosition()
+    private (bool success, RaycastHit hitInfo) GetMousePosition()
     {
-        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, groundMask))
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
         {
-            // The Raycast hit something, return with the position.
-            return (success: true, position: hitInfo.point);
+            return (true, hit);
         }
-        else
-        {
-            // The Raycast did not hit anything.
-            return (success: false, position: Vector3.zero);
-        }
+
+        return (false, default(RaycastHit));
     }
 }
