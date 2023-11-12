412e5abb32d9628e553d62ca0022e262b80e9618
Jerry Chen <jerry.w.chen.official@gmail.com>
2023-11-12 17:25:35 -0500

    encircle full implementation

--- Assets/Scripts/Enemy/EncircleEnemy.cs
+++ Assets/Scripts/Enemy/EncircleEnemy.cs
@@ -9,8 +9,6 @@ public class EncircleEnemy : Enemy
   [SerializeField] private LineRenderer lineRenderer;
   [SerializeField] private int segments = 50; // Number of segments to create a circle
   [SerializeField] private Material attackMaterial; // Material used when attacking
-  [SerializeField] private float attackDelay = 1f;
-  [SerializeField] private float hitboxActiveTime = 2f;
 
   private float angle;
   private bool firstCircleCompleted = false;
@@ -31,53 +29,57 @@ public class EncircleEnemy : Enemy
   protected override void Update()
   {
     base.Update();
-
-    if (state == EnemyState.CHASE || state == EnemyState.ATTACK)
+    // if whithin radius, attack:
+    if (Vector3.Distance(transform.position, targetTransform.position) <= encircleRadius)
     {
+      state = EnemyState.ATTACK;
       EncircleTarget();
     }
   }
+
+  private Vector3 startPosition;
+  [SerializeField] private float returnErrorMargin = 1.0f; // Error margin for returning to the start position
 
   private void EncircleTarget()
   {
     if (targetTransform != null)
     {
+      // Calculate the angle increment based on time and encircle speed
       angle += encircleSpeed * Time.deltaTime;
 
+      // Calculate the direction vector from the target to the enemy and rotate it
       Vector3 direction = (transform.position - targetTransform.position).normalized;
       direction = Quaternion.Euler(0, angle, 0) * direction;
       Vector3 newPosition = targetTransform.position + direction * encircleRadius;
 
+      // Set the NavMeshAgent's destination to the new position
       agent.SetDestination(newPosition);
 
-      if (!isDrawingCircle)
+      // Check if the enemy has returned to the near vicinity of the start position
+      if (Vector3.Distance(startPosition, transform.position) <= returnErrorMargin && angle >= 360f)
       {
-        DrawCirclePath();
-      }
-
-      if (angle >= 360f)
-      {
-        if (firstCircleCompleted)
-        {
-          StartCoroutine(PerformAttack());
-        }
-        else
-        {
-          firstCircleCompleted = true;
-        }
-        angle -= 360f; // Reset the angle for the next encircling
+        StartCoroutine(PerformAttack());
+        angle = 0; // Reset the angle for the next encircling
       }
     }
   }
+
+
+
 
   private void DrawCirclePath()
   {
+    lineRenderer.useWorldSpace = true; // Use world space for correct positioning
+    Vector3 enemyPosition = transform.position;
+    float yOffset = -0.5f; // Adjust this value to set how far below the enemy the line should be drawn
+
     for (int i = 0; i <= segments; i++)
     {
       float radian = 2 * Mathf.PI * i / segments;
-      lineRenderer.SetPosition(i, new Vector3(Mathf.Sin(radian) * encircleRadius, 0, Mathf.Cos(radian) * encircleRadius));
+      lineRenderer.SetPosition(i, new Vector3(enemyPosition.x + Mathf.Sin(radian) * encircleRadius, enemyPosition.y - yOffset, enemyPosition.z + Mathf.Cos(radian) * encircleRadius));
     }
   }
+
 
   IEnumerator PerformAttack()
   {
